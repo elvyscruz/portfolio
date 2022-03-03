5fe564c59ed1d26eb152360e38fa13fd79578b19
elvyscruz <elvys.cruz@gmail.com>
2022-03-02 23:52:38 -0400

    changed binary tree components's title

--- src/App.tsx
+++ src/App.tsx
@@ -12,7 +12,7 @@ function App() {
       <h1>Interactive Portfolio</h1>
       <hr />
       <P5Canvas title="Singly Linked List" id="linkedList" sketch={linkedList} />
-      <P5Canvas title="DFS and BFS Binary Trees" id="binaryTree" sketch={binaryTree} />
+      <P5Canvas title="DFS and BFS Binary Tree Traversal" id="binaryTree" sketch={binaryTree} />
       <P5Canvas title="Terrain Generator" id="terrainGenerator" sketch={terrainGenerator} />
       <P5Canvas title="Gravity, Mass and Acceleration" id="gravity" sketch={gravity} />
 
